c4931c47dd69561e24b66232c11284f9c807a855
akshitha <akshithas.bsc23@rvu.edu.in>
2024-02-17 20:49:20 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -3,14 +3,6 @@
 void input(float *x1, float *y1, float *x2, float *y2);
 float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
-int main()
-{
-    float x1, y1, x2, y2, distance;
-    input(&x1, &y1, &x2, &y2); 
-    distance = find_distance(x1, y1, x2, y2);
-    output(x1, y1, x2, y2, distance); 
-    return 0;
-}
 void input(float *x1, float *y1, float *x2, float *y2)
 {
     printf("Enter the coordinates of the first point: ");
@@ -28,4 +20,11 @@ void output(float x1, float y1, float x2, float y2, float distance)
 {
     printf("The distance between the points (%.2f, %.2f) and (%.2f, %.2f) is %.2f units.\n", x1, y1, x2, y2, distance);
 }
-
+int main()
+{
+    float x1, y1, x2, y2, distance;
+    input(&x1, &y1, &x2, &y2); 
+    distance = find_distance(x1, y1, x2, y2);
+    output(x1, y1, x2, y2, distance); 
+    return 0;
+}
